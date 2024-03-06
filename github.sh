27300130b27df80adce7fb3838918aafd85892c5
Joshua K. Stewart <jks403@humboldt.edu>
2024-03-06 06:53:11 +0000

    adding github.sh

---
+++ github.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+cd /var/www/app/
+git pull origin main --ff-only
+
+# this says to change to the /var/www/app/ directory 
+# this tells the computer to run from this directory our current script
+# current script is in cd /var/www/app/
+
+
