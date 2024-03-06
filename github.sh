7fc2c0edbaf9801774f7e6c36c62b1fc1c4c043e
Joshua K. Stewart <30303947+jks403@users.noreply.github.com>
2024-03-06 01:37:11 -0800

    Update github.sh
    pulling from Master and Main branches and piping to github.sh

--- github.sh
+++ github.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/bash
 cd /var/www/app/
 git pull origin main --ff-only
+git pull origin master --ff-only
 
 # this says to change to the /var/www/app/ directory 
 # this tells the computer to run from this directory our current script
