a7c191f138012e5bf83cc547111c0fe373c0a691
Joshua K. Stewart <30303947+jks403@users.noreply.github.com>
2024-03-05 23:58:19 -0800

    Update app.js
    added app.js for the current date

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const http = require('http');
 
 http.createServer(function(req, res) {
-res.write("This is the Planned Parenthood employee application. It's a full stack application");
+res.write("This is March 5th, 2024 11:57 PM. PPNorCal Employee Database and web application. It's a full stack application");
 	res.end();
 
 	
