ae0e052ed3853aa3ede2dbba7637d8286892113d
jks403@humboldt.edu <jks403@humboldt.edu>
2024-03-13 06:12:33 +0000

    adding http

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // create a new server 
 //
-const https = require('https');
+const http = require('http');
 
-https.createServer(function(req, res) {
+http.createServer(function(req, res) {
 res.write(" The time is March 11th 10:49 pm. Just did 4 monitor installs and checked serial numbers . PPNorCal Employee Database and web application. It's a full stack application");
 	res.end();
 
